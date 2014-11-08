feb167e5d1ffec218b3fe7d2257a82c6635f0a7f
Arseniy Pendryak <arseniy1993@mail.ru>
2014-11-08 03:49:57 +0300

    created abstract JSON creator

---
+++ src/main/java/com/ifmo/recommendersystem/AbstractJSONCreator.java
@@ -0,0 +1,17 @@
+package com.ifmo.recommendersystem;
+
+import org.json.JSONObject;
+
+public abstract class AbstractJSONCreator<T extends JSONConverted> implements JSONConverted.JSONCreator<T>  {
+
+    @Override
+    public T fromJSON(JSONObject jsonObject) {
+        try {
+            return throwableFromJSON(jsonObject);
+        } catch (Exception e) {
+            throw new JSONConverted.IllegalJSONFormatException(e);
+        }
+    }
+
+    protected abstract T throwableFromJSON(JSONObject jsonObject) throws Exception;
+}
