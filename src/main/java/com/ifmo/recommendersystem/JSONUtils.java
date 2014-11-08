2aa18cc2cab074aa5e49d16394b2871853de2220
Arseniy Pendryak <arseniy1993@mail.ru>
2014-11-08 03:58:13 +0300

    created inner JSON utils

---
+++ src/main/java/com/ifmo/recommendersystem/JSONUtils.java
@@ -0,0 +1,67 @@
+package com.ifmo.recommendersystem;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import weka.core.matrix.Matrix;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+class JSONUtils {
+
+    public static final String ALPHA = "alpha";
+    public static final String BETTA = "betta";
+    public static final String CLASSIFIER = "classifier";
+    public static final String ALGORITHMS = "algorithms";
+    public static final String DATA_SETS = "dataSets";
+    public static final String SEARCH = "search";
+    public static final String EVALUATION = "evaluation";
+    public static final String OPTIONS = "options";
+    public static final String NAME = "name";
+    public static final String META_FEATURES = "metaFeatures";
+    public static final String EARR_MATRIX = "earrMatrix";
+
+    public static String[] readOptions(JSONObject jsonObject) {
+        if (jsonObject.has(OPTIONS)) {
+            JSONArray optionsArray = jsonObject.getJSONArray(OPTIONS);
+            String[] options = new String[optionsArray.length()];
+            for (int i = 0; i < optionsArray.length(); i++) {
+                options[i] = optionsArray.getString(i);
+            }
+            return options;
+        }
+        return null;
+    }
+
+    public static JSONObject objectToJSON(Object object, String[] options) {
+        return new JSONObject().
+                put(NAME, object.getClass().getCanonicalName()).
+                put(OPTIONS, options == null ? Collections.emptyList() : Arrays.asList(options));
+    }
+
+    public static JSONArray collectionToJSONArray(Collection<? extends JSONConverted> collection) {
+        List<JSONObject> objectList = new ArrayList<>(collection.size());
+        objectList.addAll(collection.stream().map(JSONConverted::toJSON).collect(Collectors.toList()));
+        return new JSONArray(objectList);
+    }
+
+    public static <T> List<T> jsonArrayToObjectList(JSONArray jsonArray, JSONConverted.JSONCreator<T> creator) {
+        List<T> objects = new ArrayList<>(jsonArray.length());
+        for (int i = 0; i < jsonArray.length(); i++) {
+            objects.add(creator.fromJSON(jsonArray.getJSONObject(i)));
+        }
+        return objects;
+    }
+
+    public static JSONArray earrMatrixToJSONArray(Matrix earrMatrix) {
+        List<JSONArray> rows = new ArrayList<>(earrMatrix.getRowDimension());
+        for (int i = 0; i < earrMatrix.getRowDimension(); i++) {
+            List<Double> row = new ArrayList<>(earrMatrix.getColumnDimension());
+            for (int j = 0; j < earrMatrix.getColumnDimension(); j++) {
+                row.add(earrMatrix.get(i, j));
+            }
+            rows.add(new JSONArray(row));
+        }
+        return new JSONArray(rows);
+    }
+}
