e22553dd473bc7e7615973dccd769da295f3c9c2
Arseniy Pendryak <arseniy1993@mail.ru>
2014-11-19 17:57:39 +0300

    added name. static import JSONUtils

--- src/main/java/com/ifmo/recommendersystem/FSSAlgorithm.java
+++ src/main/java/com/ifmo/recommendersystem/FSSAlgorithm.java
@@ -4,8 +4,8 @@ import org.json.JSONObject;
 import weka.attributeSelection.ASEvaluation;
 import weka.attributeSelection.ASSearch;
 import weka.core.Instances;
-import weka.filters.Filter;
-import weka.filters.supervised.attribute.AttributeSelection;
+
+import static com.ifmo.recommendersystem.JSONUtils.*;
 
 public class FSSAlgorithm implements JSONConverted {
 
@@ -19,13 +19,15 @@ public class FSSAlgorithm implements JSONConverted {
         }
     }
 
+    public final String name;
     public final ASSearch algorithm;
     public final String[] algorithmOptions;
     public final ASEvaluation evaluator;
     public final String[] evaluationOptions;
 
-    private FSSAlgorithm(ASSearch algorithm, String[] algorithmOptions,
+    private FSSAlgorithm(String name, ASSearch algorithm, String[] algorithmOptions,
                          ASEvaluation evaluator, String[] evaluationOptions) {
+        this.name = name;
         this.algorithm = algorithm;
         this.algorithmOptions = algorithmOptions;
         this.evaluator = evaluator;
@@ -35,13 +37,9 @@ public class FSSAlgorithm implements JSONConverted {
     public Result subsetSelection(Instances instances) {
         try {
             ASSearch search = ASSearch.makeCopies(algorithm, 1)[0];
-            ASEvaluation localEvaluator = ASEvaluation.makeCopies(evaluator, 1)[0];
-            AttributeSelection filter = new AttributeSelection();
-            filter.setSearch(search);
-            filter.setEvaluator(localEvaluator);
-            filter.setInputFormat(instances);
+            ASEvaluation evaluation = ASEvaluation.makeCopies(evaluator, 1)[0];
             long start = System.currentTimeMillis();
-            Instances resultInstances = Filter.useFilter(instances, filter);
+            Instances resultInstances = InstancesUtils.selectAttributes(instances, search, evaluation);
             long end = System.currentTimeMillis();
             return new Result(resultInstances, end - start);
         } catch (Exception e) {
@@ -66,25 +64,30 @@ public class FSSAlgorithm implements JSONConverted {
         return evaluationOptions;
     }
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public JSONObject toJSON() {
-        JSONObject search = JSONUtils.objectToJSON(algorithm, algorithmOptions);
-        JSONObject evaluation = JSONUtils.objectToJSON(evaluator, evaluationOptions);
-        return new JSONObject().put(JSONUtils.SEARCH, search).put(JSONUtils.EVALUATION, evaluation);
+        JSONObject search = objectToJSON(algorithm, algorithmOptions);
+        JSONObject evaluation = objectToJSON(evaluator, evaluationOptions);
+        return new JSONObject().put(NAME, name).put(SEARCH, search).put(EVALUATION, evaluation);
     }
 
     public static final AbstractJSONCreator<FSSAlgorithm> JSON_CREATOR = new AbstractJSONCreator<FSSAlgorithm>() {
         @Override
         protected FSSAlgorithm throwableFromJSON(JSONObject jsonObject) throws Exception {
-            JSONObject search = jsonObject.getJSONObject(JSONUtils.SEARCH);
-            JSONObject evaluation = jsonObject.getJSONObject(JSONUtils.EVALUATION);
-            String algorithmClassName = search.getString(JSONUtils.NAME);
-            String evaluationClassName = evaluation.getString(JSONUtils.NAME);
-            String[] algorithmOptions = JSONUtils.readOptions(search);
-            String[] evaluationOptions = JSONUtils.readOptions(evaluation);
+            String name = jsonObject.getString(NAME);
+            JSONObject search = jsonObject.getJSONObject(SEARCH);
+            JSONObject evaluation = jsonObject.getJSONObject(EVALUATION);
+            String algorithmClassName = search.getString(CLASS_NAME);
+            String evaluationClassName = evaluation.getString(CLASS_NAME);
+            String[] algorithmOptions = readOptions(search);
+            String[] evaluationOptions = readOptions(evaluation);
             ASSearch algorithm = ASSearch.forName(algorithmClassName, algorithmOptions);
             ASEvaluation evaluator = ASEvaluation.forName(evaluationClassName, evaluationOptions);
-            return new FSSAlgorithm(algorithm, algorithmOptions, evaluator, evaluationOptions);
+            return new FSSAlgorithm(name, algorithm, algorithmOptions, evaluator, evaluationOptions);
         }
     };
 }
