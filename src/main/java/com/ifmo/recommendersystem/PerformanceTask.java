b17579234422fdee64cff6ba1a34d218f75dd351
Arseniy Pendryak <arseniy1993@mail.ru>
2014-11-19 20:59:51 +0300

    new PerformanceTask

--- src/main/java/com/ifmo/recommendersystem/PerformanceTask.java
+++ src/main/java/com/ifmo/recommendersystem/PerformanceTask.java
@@ -1,13 +1,11 @@
 package com.ifmo.recommendersystem;
 
-import org.json.JSONArray;
 import org.json.JSONObject;
 import weka.core.Instances;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.List;
 import java.util.Random;
 
 import static com.ifmo.recommendersystem.JSONUtils.*;
@@ -22,13 +20,16 @@ public class PerformanceTask extends AbstractTask {
     private final static int ROUNDS = 5;
     private final static int FOLDS = 10;
 
-    private final List<FSSAlgorithm> algorithms;
+    private final String name;
+
+    private final FSSAlgorithm algorithm;
     private final ClassifierWrapper classifier;
 
-    public PerformanceTask(String datasetPath, List<FSSAlgorithm> algorithms, ClassifierWrapper classifier) {
+    public PerformanceTask(String datasetPath, FSSAlgorithm algorithm, ClassifierWrapper classifier) {
         super(datasetPath);
-        this.algorithms = algorithms;
+        this.algorithm = algorithm;
         this.classifier = classifier;
+        this.name = Utils.createName(classifier.getName(), algorithm.getName(), datasetPath);
     }
 
     @Override
@@ -36,46 +37,44 @@ public class PerformanceTask extends AbstractTask {
         try {
             Instances instances = InstancesUtils.createInstances(datasetPath, true);
             Random random = new Random();
-            int algorithmNumber = algorithms.size();
-            double[] runtime = new double[algorithmNumber];
-            double[] accuracy = new double[algorithmNumber];
-            double[] resultAttributeNumber = new double[algorithmNumber];
+            double runtime = 0;
+            double accuracy = 0;
+            double resultAttributeNumber = 0;
             for (int i = 0; i < ROUNDS; i++) {
                 instances.randomize(random);
                 for (int j = 0; j < FOLDS; j++) {
-                    for (int k = 0; k < algorithmNumber; k++) {
-                        Instances train = instances.trainCV(FOLDS, j);
-                        Instances test = instances.testCV(FOLDS, j);
-                        FSSAlgorithm.Result result = algorithms.get(k).subsetSelection(train);
-                        Instances selectedTrain = InstancesUtils.removeAttributes(train, result.instances);
-                        Instances selectedTest = InstancesUtils.removeAttributes(test, result.instances);
-                        runtime[k] += result.runtime;
-                        resultAttributeNumber[k] += selectedTrain.numAttributes() - 1;
-                        accuracy[k] += classifier.getAccuracy(selectedTrain, selectedTest);
-                    }
+                    Instances train = instances.trainCV(FOLDS, j);
+                    Instances test = instances.testCV(FOLDS, j);
+                    FSSAlgorithm.Result result = algorithm.subsetSelection(train);
+                    Instances selectedTrain = InstancesUtils.removeAttributes(train, result.instances);
+                    Instances selectedTest = InstancesUtils.removeAttributes(test, result.instances);
+                    runtime += result.runtime;
+                    resultAttributeNumber += selectedTrain.numAttributes() - 1;
+                    accuracy += classifier.getAccuracy(selectedTrain, selectedTest);
                 }
             }
-            for (int i = 0; i < algorithmNumber; i++) {
-                runtime[i] /= ROUNDS * FOLDS;
-                accuracy[i] /= ROUNDS * FOLDS;
-                resultAttributeNumber[i] /= ROUNDS * FOLDS;
-            }
 
-            JSONArray array = new JSONArray();
-            for (int i = 0; i < algorithmNumber; i++) {
-                array.put(new JSONObject().
-                        put(ALGORITHM, algorithms.get(i).getName()).
-                        put(MEAN_RUNTIME, runtime[i]).
-                        put(MEAN_ACCURACY, accuracy[i]).
-                        put(MEAN_ATTRIBUTE_NUMBER, resultAttributeNumber[i]));
-            }
+            runtime /= ROUNDS * FOLDS;
+            accuracy /= ROUNDS * FOLDS;
+            resultAttributeNumber /= ROUNDS * FOLDS;
 
             JSONObject result = new JSONObject().
-                    put(NAME, instances.relationName()).
-                    put(RESULTS, array);
-            File directory = new File(RESULT_DIRECTORY, PERFORMANCE_DIRECTORY + File.separator + classifier.getName());
+                    put(DATA_SET_NAME, instances.relationName()).
+                    put(ALGORITHM_NAME, algorithm.getName()).
+                    put(CLASS_NAME, classifier.getName()).
+                    put(MEAN_RUNTIME, runtime).
+                    put(MEAN_ACCURACY, accuracy).
+                    put(MEAN_ATTRIBUTE_NUMBER, resultAttributeNumber);
+
+            String directoryPath = Utils.createPath(RESULT_DIRECTORY,
+                    PERFORMANCE_DIRECTORY,
+                    classifier.getName(),
+                    instances.relationName(),
+                    algorithm.getName());
+            String fileName = Utils.createName(classifier.getName(), instances.relationName(), algorithm.getName());
+            File directory = new File(directoryPath);
             directory.mkdirs();
-            try (PrintWriter writer = new PrintWriter(new File(directory, instances.relationName() + ".json"))) {
+            try (PrintWriter writer = new PrintWriter(new File(directory, fileName + ".json"))) {
                 writer.print(result.toString(4));
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -87,7 +86,8 @@ public class PerformanceTask extends AbstractTask {
     }
 
     @Override
-    protected String getTaskType() {
-        return PERFORMANCE_TYPE;
+    protected String getTaskName() {
+        return name;
     }
+
 }
