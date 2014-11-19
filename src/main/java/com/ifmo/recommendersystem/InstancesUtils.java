4066cb0b43322cca4a2caff10c961809eb14629d
Arseniy Pendryak <arseniy1993@mail.ru>
2014-11-19 17:59:20 +0300

    created selectAttributes(), removeAttributes() and printAttr() methods

--- src/main/java/com/ifmo/recommendersystem/InstancesUtils.java
+++ src/main/java/com/ifmo/recommendersystem/InstancesUtils.java
@@ -1,8 +1,17 @@
 package com.ifmo.recommendersystem;
 
+import gnu.trove.list.TIntList;
+import gnu.trove.list.array.TIntArrayList;
+import weka.attributeSelection.ASEvaluation;
+import weka.attributeSelection.ASSearch;
 import weka.core.Attribute;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils;
+import weka.filters.Filter;
+import weka.filters.supervised.attribute.AttributeSelection;
+import weka.filters.unsupervised.attribute.Remove;
+
+import java.util.*;
 
 public class InstancesUtils {
 
@@ -29,12 +38,49 @@ public class InstancesUtils {
             if (copy) {
                 instances = new Instances(instances);
             }
-            for (int i = instances.numAttributes() - 1; i >= 0; i--) {
-                if (instances.attribute(i).isString()) {
-                    instances.deleteAttributeAt(i);
-                }
-            }
+            instances.deleteStringAttributes();
         }
         return instances;
+    }
+
+    public static Instances selectAttributes(Instances instances, ASSearch search, ASEvaluation evaluation) {
+        AttributeSelection filter = new AttributeSelection();
+        filter.setSearch(search);
+        filter.setEvaluator(evaluation);
+        try {
+            filter.setInputFormat(instances);
+            return Filter.useFilter(instances, filter);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Instances removeAttributes(Instances intstances, Instances pattern) {
+        Set<String> attributes = new HashSet<>(pattern.numAttributes());
+        for (int i = 0; i < pattern.numAttributes(); i++) {
+            attributes.add(pattern.attribute(i).name());
+        }
+        TIntList removingAttributes = new TIntArrayList(intstances.numAttributes() - pattern.numAttributes());
+        for (int i = 0; i < intstances.numAttributes(); i++) {
+            if (!attributes.contains(intstances.attribute(i).name())) {
+                removingAttributes.add(i);
+            }
+        }
+        Remove remove = new Remove();
+        try {
+            remove.setAttributeIndicesArray(removingAttributes.toArray());
+            remove.setInputFormat(intstances);
+            return Filter.useFilter(intstances, remove);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    static void printAttr(Instances instances) {
+        for (int i = 0; i < instances.numAttributes(); i++) {
+            System.out.println(instances.attribute(i).name());
+        }
     }
 }
