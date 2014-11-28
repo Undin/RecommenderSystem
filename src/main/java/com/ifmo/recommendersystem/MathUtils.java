337db7e7d7aeac7e3338bfc0711314274e12f7cd
Arseniy Pendryak <arseniy1993@mail.ru>
2014-11-28 17:21:50 +0300

    support missing values. added covariance method

--- src/main/java/com/ifmo/recommendersystem/MathUtils.java
+++ src/main/java/com/ifmo/recommendersystem/MathUtils.java
@@ -1,13 +1,19 @@
 package com.ifmo.recommendersystem;
 
+import weka.core.Instance;
+
 public class MathUtils {
 
     public static double rawMoment(double[] values, int k) {
         double acc = 0;
+        double count = 0;
         for (double value : values) {
-            acc += StrictMath.pow(value, k);
+            if (isCorrectValue(value)) {
+                acc += StrictMath.pow(value, k);
+                count++;
+            }
         }
-        return acc / values.length;
+        return acc / count;
     }
 
     public static double centralMoment(double[] values, int k, double mean) {
@@ -33,8 +39,38 @@ public class MathUtils {
     private static double[] unshifted(double[] values, double mean) {
         double[] tmp = new double[values.length];
         for (int i = 0; i < values.length; i++) {
-            tmp[i] = values[i] - mean;
+            if (isCorrectValue(values[i])) {
+                tmp[i] = values[i] - mean;
+            }
         }
         return tmp;
+    }
+
+    public static double covariance(double values1[], double values2[], double mean1, double mean2) {
+        if (values1.length != values2.length) {
+            throw new IllegalArgumentException();
+        }
+        int length = values1.length;
+        double cov = 0;
+        double count = 0;
+        for (int i = 0; i < length; i++) {
+            if (isCorrectValue(values1[i]) && isCorrectValue(values2[i])) {
+                cov += (values1[i] - mean1) * (values2[i] - mean2);
+                count++;
+            }
+        }
+        cov /= count;
+        return cov;
+    }
+
+    public static double covariance(double values1[], double values2[]) {
+        if (values1.length != values2.length) {
+            throw new IllegalArgumentException();
+        }
+        return covariance(values1, values2, mean(values1), mean(values2));
+    }
+
+    public static boolean isCorrectValue(double v) {
+        return !Instance.isMissingValue(v);
     }
 }
