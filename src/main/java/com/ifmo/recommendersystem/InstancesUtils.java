92f728d725f79d7e2f850c9dc2f9c36deccd3a49
Arseniy Pendryak <arseniy1993@mail.ru>
2014-11-28 17:29:52 +0300

    added options for insctance creation

--- src/main/java/com/ifmo/recommendersystem/InstancesUtils.java
+++ src/main/java/com/ifmo/recommendersystem/InstancesUtils.java
@@ -2,6 +2,8 @@ package com.ifmo.recommendersystem;
 
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
+import gnu.trove.set.TIntSet;
+import gnu.trove.set.hash.TIntHashSet;
 import weka.attributeSelection.ASEvaluation;
 import weka.attributeSelection.ASSearch;
 import weka.core.Attribute;
@@ -17,30 +19,36 @@ public class InstancesUtils {
 
     private static final String CLASS_ATTRIBUTE_NAME = "class";
 
-    public static Instances createInstances(String filename, boolean removeStringAttributes) throws Exception {
+    public static final int REMOVE_STRING_ATTRIBUTES = 1;
+    public static final int REMOVE_UNINFORMATIVE_ATTRIBUTES = 2; // variance == 0
+
+    public static Instances createInstances(String filename, int modifiedFlags) throws Exception {
         Instances instances = ConverterUtils.DataSource.read(filename);
-        if (removeStringAttributes) {
-            instances = removeStringAttributes(instances, false);
-        }
         Attribute attribute = instances.attribute(CLASS_ATTRIBUTE_NAME);
         if (attribute != null) {
             instances.setClassIndex(attribute.index());
+        }
+        boolean removeStringAttributes = (modifiedFlags & REMOVE_STRING_ATTRIBUTES) != 0;
+        boolean removeUninformativeAttributes = (modifiedFlags & REMOVE_UNINFORMATIVE_ATTRIBUTES) != 0;
+        if (removeStringAttributes || removeUninformativeAttributes) {
+            TIntSet removingAttributes = new TIntHashSet();
+            for (int i = 0; i < instances.numAttributes(); i++) {
+                Attribute attr = instances.attribute(i);
+                if (removeStringAttributes && attr.isString()) {
+                    removingAttributes.add(i);
+                }
+                if (removeUninformativeAttributes && MathUtils.variance(instances.attributeToDoubleArray(i)) == 0) {
+                    removingAttributes.add(i);
+                }
+            }
+
+            instances = removeAttribute(instances, removingAttributes.toArray());
         }
         return instances;
     }
 
     public static Instances createInstances(String filename) throws Exception {
-        return createInstances(filename, false);
-    }
-
-    public static Instances removeStringAttributes(Instances instances, boolean copy) {
-        if (instances.checkForStringAttributes()) {
-            if (copy) {
-                instances = new Instances(instances);
-            }
-            instances.deleteStringAttributes();
-        }
-        return instances;
+        return createInstances(filename, 0);
     }
 
     public static Instances selectAttributes(Instances instances, ASSearch search, ASEvaluation evaluation) {
@@ -56,22 +64,26 @@ public class InstancesUtils {
         }
     }
 
-    public static Instances removeAttributes(Instances intstances, Instances pattern) {
+    public static Instances removeAttributes(Instances instances, Instances pattern) {
         Set<String> attributes = new HashSet<>(pattern.numAttributes());
         for (int i = 0; i < pattern.numAttributes(); i++) {
             attributes.add(pattern.attribute(i).name());
         }
-        TIntList removingAttributes = new TIntArrayList(intstances.numAttributes() - pattern.numAttributes());
-        for (int i = 0; i < intstances.numAttributes(); i++) {
-            if (!attributes.contains(intstances.attribute(i).name())) {
+        TIntList removingAttributes = new TIntArrayList(instances.numAttributes() - pattern.numAttributes());
+        for (int i = 0; i < instances.numAttributes(); i++) {
+            if (!attributes.contains(instances.attribute(i).name())) {
                 removingAttributes.add(i);
             }
         }
+        return removeAttribute(instances, removingAttributes.toArray());
+    }
+
+    private static Instances removeAttribute(Instances instances, int[] removingAttributes) {
         Remove remove = new Remove();
         try {
-            remove.setAttributeIndicesArray(removingAttributes.toArray());
-            remove.setInputFormat(intstances);
-            return Filter.useFilter(intstances, remove);
+            remove.setAttributeIndicesArray(removingAttributes);
+            remove.setInputFormat(instances);
+            return Filter.useFilter(instances, remove);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
