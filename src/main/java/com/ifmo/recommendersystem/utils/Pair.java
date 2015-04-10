3e4414d1c98e3567d5f689169cb34ec6bdafcd4f
Arseniy Pendryak <arseniy1993@mail.ru>
2015-04-10 15:11:26 +0300

    created Pair class

---
+++ src/main/java/com/ifmo/recommendersystem/utils/Pair.java
@@ -0,0 +1,36 @@
+package com.ifmo.recommendersystem.utils;
+
+public class Pair<F, S> {
+
+    public final F first;
+    public final S second;
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+
+        Pair pair = (Pair) o;
+
+        if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+        if (second != null ? !second.equals(pair.second) : pair.second != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (second != null ? second.hashCode() : 0);
+        return result;
+    }
+
+    public static <F, S> Pair<F, S> of(F first, S second) {
+        return new Pair<>(first, second);
+    }
+}
