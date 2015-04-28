be595da490749cf4c5253ed6f7d99cb60965de70
Arseniy Pendryak <arseniy1993@mail.ru>
2015-04-28 05:08:59 +0300

    added attribute reordering

--- src/main/java/com/ifmo/recommendersystem/utils/InstancesUtils.java
+++ src/main/java/com/ifmo/recommendersystem/utils/InstancesUtils.java
@@ -1,6 +1,5 @@
 package com.ifmo.recommendersystem.utils;
 
-import com.ifmo.recommendersystem.utils.StatisticalUtils;
 import gnu.trove.list.TIntList;
 import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.set.TIntSet;
@@ -14,8 +13,12 @@ import weka.filters.Filter;
 import weka.filters.supervised.attribute.AttributeSelection;
 import weka.filters.supervised.attribute.Discretize;
 import weka.filters.unsupervised.attribute.Remove;
+import weka.filters.unsupervised.attribute.Reorder;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InstancesUtils {
 
@@ -28,7 +31,15 @@ public class InstancesUtils {
         Instances instances = ConverterUtils.DataSource.read(filename);
         Attribute attribute = instances.attribute(CLASS_ATTRIBUTE_NAME);
         if (attribute != null) {
-            instances.setClassIndex(attribute.index());
+            int classIndex = attribute.index() + 1;
+            if (classIndex != instances.numAttributes() - 1) {
+                int[] order = IntStream.concat(
+                        IntStream.range(1, instances.numAttributes() + 1)
+                                .filter(i -> i != classIndex),
+                        IntStream.of(classIndex)).toArray();
+                instances = reorder(instances, order);
+            }
+            instances.setClassIndex(instances.numAttributes() - 1);
         }
         boolean removeStringAttributes = (modifiedFlags & REMOVE_STRING_ATTRIBUTES) != 0;
         boolean removeUninformativeAttributes = (modifiedFlags & REMOVE_UNINFORMATIVE_ATTRIBUTES) != 0;
@@ -72,6 +83,21 @@ public class InstancesUtils {
         try {
             discretize.setInputFormat(instances);
             return Filter.useFilter(instances, discretize);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static Instances reorder(Instances instances, int[] order) {
+        String newOrder = String.join(",", IntStream.of(order)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.toList()));
+        try {
+            Reorder reorder = new Reorder();
+            reorder.setOptions(new String[]{"-R", newOrder});
+            reorder.setInputFormat(instances);
+            return Filter.useFilter(instances, reorder);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
