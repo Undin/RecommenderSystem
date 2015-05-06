c02ed265fd8e4633d036ab84cee8ac39926761e2
Arseniy Pendryak <arseniy1993@mail.ru>
2015-05-06 10:45:48 +0300

    created meta feature converter

---
+++ src/main/java/com/ifmo/recommendersystem/MetaFeatureConverter.java
@@ -0,0 +1,60 @@
+package com.ifmo.recommendersystem;
+
+import com.ifmo.recommendersystem.config.Config;
+import com.ifmo.recommendersystem.config.EvaluationConfig;
+import com.ifmo.recommendersystem.tasks.ExtractResult;
+import com.ifmo.recommendersystem.utils.JSONUtils;
+import weka.core.Attribute;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+/**
+ * Created by warrior on 06.05.15.
+ */
+public class MetaFeatureConverter {
+
+    public static final String META_FEATURE_RELATION_NAME = "metaFeatures";
+
+    public static void main(String[] args) {
+        EvaluationConfig config = new EvaluationConfig("evaluationConfig.json");
+        toArff(config, RecommenderSystemBuilder.META_FEATURES_DIRECTORY, META_FEATURE_RELATION_NAME);
+    }
+
+    public static void toArff(Config config, String sourceDirectory, String relationName) {
+        Instances instances = createInstances(config, sourceDirectory, relationName);
+        try (PrintWriter writer = new PrintWriter("metaFeatures.arff")) {
+            writer.println(instances.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Instances createInstances(Config config, String sourceDirectory, String relationName) {
+        ArrayList<Attribute> attributes = config.getExtractors().stream()
+                .map(e -> new Attribute(e.getName()))
+                .collect(Collectors.toCollection(ArrayList::new));
+        int capacity = config.getDatasets().size();
+        Instances instances = new Instances(relationName, attributes, capacity);
+
+        File file = new File(sourceDirectory);
+        for (String dataset : config.getDatasets()) {
+            File datasetDir = new File(file, dataset);
+
+            double[] values = config.getExtractors().stream()
+                    .mapToDouble(e -> {
+                        File metaFeatureFile = new File(datasetDir, e.getClass().getCanonicalName() + ".json");
+                        ExtractResult result = ExtractResult.JSON_CREATOR.fromJSON(JSONUtils.readJSONObject(metaFeatureFile.getAbsolutePath()));
+                        return result.getMetaFeature().getValue();
+                    })
+                    .toArray();
+            instances.add(new DenseInstance(1, values));
+        }
+        return instances;
+    }
+}
