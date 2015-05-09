1e04b52bf714b42c98d3c36ce47b9153e8e02274
Arseniy Pendryak <arseniy1993@mail.ru>
2015-05-09 17:13:26 +0300

    created evaluateSubset(int[])

--- src/main/java/com/ifmo/recommendersystem/evaluation/AbstractRecommenderSystemEvaluator.java
+++ src/main/java/com/ifmo/recommendersystem/evaluation/AbstractRecommenderSystemEvaluator.java
@@ -4,10 +4,19 @@ import weka.attributeSelection.UnsupervisedSubsetEvaluator;
 import weka.clusterers.Clusterer;
 import weka.core.Instances;
 
+import java.util.BitSet;
+import java.util.stream.IntStream;
+
 /**
  * Created by warrior on 08.05.15.
  */
 public abstract class AbstractRecommenderSystemEvaluator extends UnsupervisedSubsetEvaluator {
+
+    public double evaluateSubset(int[] subset) throws Exception {
+        BitSet bitSet = new BitSet();
+        IntStream.of(subset).forEach(bitSet::set);
+        return evaluateSubset(bitSet);
+    }
 
     @Override
     public void buildEvaluator(Instances data) throws Exception {
