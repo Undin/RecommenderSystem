be17bc0c085212bbac899dd29dc64493bc386c3e
Arseniy Pendryak <arseniy1993@mail.ru>
2015-06-07 22:56:56 +0300

    ROUNDS = 10

--- src/main/java/com/ifmo/recommendersystem/metafeatures/classifierbased/AbstractClassifierBasedExtractor.java
+++ src/main/java/com/ifmo/recommendersystem/metafeatures/classifierbased/AbstractClassifierBasedExtractor.java
@@ -8,7 +8,7 @@ import weka.core.Instances;
  */
 public abstract class AbstractClassifierBasedExtractor extends MetaFeatureExtractor {
 
-    private static final int ROUNDS = 50;
+    private static final int ROUNDS = 10;
 
     private final Extractor extractor;
     private final Transform transform;
