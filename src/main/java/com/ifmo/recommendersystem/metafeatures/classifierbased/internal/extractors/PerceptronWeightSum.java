ebaca5f392e82b031f4d1f9188ec28e1338e6ecf
Arseniy Pendryak <arseniy1993@mail.ru>
2015-06-07 23:11:28 +0300

    create PerceptronWeightSum

---
+++ src/main/java/com/ifmo/recommendersystem/metafeatures/classifierbased/internal/extractors/PerceptronWeightSum.java
@@ -0,0 +1,21 @@
+package com.ifmo.recommendersystem.metafeatures.classifierbased.internal.extractors;
+
+import com.ifmo.recommendersystem.metafeatures.classifierbased.Extractor;
+import weka.core.Instances;
+
+/**
+ * Created by warrior on 07.06.15.
+ */
+public class PerceptronWeightSum implements Extractor {
+    @Override
+    public double extract(Instances instances) {
+        MultilayerPerceptron perceptron = new MultilayerPerceptron();
+        try {
+            perceptron.buildClassifier(instances);
+            return perceptron.weightSum();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+}
